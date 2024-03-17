a2fe3f0826e2a2fa31a19b079b5687356889a83b
wyu127 <157323404+wyu127@users.noreply.github.com>
2024-03-17 10:21:09 -0400

    changed variable names from mob to villain

--- CyberGuardians/villain.gd
+++ CyberGuardians/villain.gd
@@ -2,9 +2,11 @@ extends RigidBody2D
 
 
 func _ready():
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+	var villain_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	$AnimatedSprite2D.play(villain_types[randi() % villain_types.size()])
 	
+	#$AnimatedSprite2D.animation = mob_types.pick_random()
+
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
